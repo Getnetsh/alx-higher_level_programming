193f96b90b61f917824b603d79702bbcbf2aa202
Getnetsh <Getnetss2009@gmail.com>
2023-12-18 05:58:55 -0800

    add

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print x elements of a list.
+# demonstrates how to use a try ... except for exception handling
+#
+# (C) 2023 Getnet Shiferaw
+# email Getnetss2009@gmail.com
+# -----------------------------------------------------------
+
+
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    try:
-        while i < x:
-            print("{:d}".format(my_list[i]), end="")
-            i += 1
-    except:
-        pass
+    num_of_elements = 0
+    for index in range(x):
+        try:
+            print(my_list[index], end="")
+            num_of_elements += 1
+        except IndexError:
+            break
+
     print()
-    return i
+    return num_of_elements
