b878a07af3e7bb7a604626ab1ca7606b7b7c71ed
Getnetsh <Getnetss2009@gmail.com>
2024-01-09 08:57:30 -0800

    update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""
-Class Module
-"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """
-    Student class
-    """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """initialize method
+        """Initializes a new Student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of student instance
-        args:
-            attrs: attributes
-        return:
-            dictionary
+        """Get a dictionary representation of the Student
+        If attrs is a list of strings, represents only those attributes
+        included in the list
         """
-        if not attrs:
-            return self.__dict__
-
-        return ({key: value for key, value in self.__dict__.items()
-                 if key in attrs})
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
